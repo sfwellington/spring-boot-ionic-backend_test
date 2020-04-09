b518a1588f257daec2ba354aa7ca1d7fb9f8f39b
Wellington Souza Freitas <sf.wellington@gmail.com>
2020-04-09 10:20:26 -0300

    Deletando uma categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -46,4 +46,11 @@ public class CategoriaResource {
 	}
 	
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		 service.delete(id);
+		 return ResponseEntity.noContent().build();
+	}
+	
+	
 }
