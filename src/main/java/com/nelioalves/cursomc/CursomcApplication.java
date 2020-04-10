7d0fda8cb78b4ac3d41b43fc3012bb83a35648bb
Wellington Souza Freitas <sf.wellington@gmail.com>
2020-04-10 13:56:56 -0300

    Paginando Categoria

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -63,6 +63,13 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Categoria cat3 = new Categoria(null, "Mesa ");
+		Categoria cat4 = new Categoria(null, "Categoria 4");
+		Categoria cat5 = new Categoria(null, "Categoria 5" );
+		Categoria cat6 = new Categoria(null, "Categoria 6");
+		Categoria cat7 = new Categoria(null, "Categoria 7");
+		Categoria cat8 = new Categoria(null, "Categoria 8");
+		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -74,7 +81,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 				
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
