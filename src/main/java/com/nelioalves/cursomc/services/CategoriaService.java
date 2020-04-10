d6d3fdff9a28322171a2526b76c2365d01f701b2
Wellington Souza Freitas <sf.wellington@gmail.com>
2020-04-10 13:30:17 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,4 +49,14 @@ public class CategoriaService {
 		}
 		
 	}
+	
+	
+	
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
+	
+	
 }
